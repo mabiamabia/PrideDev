9c9801a86ffd655cc0998500721dd64feb1f3eaf
Mabia <49458473+prisciladuarte@users.noreply.github.com>
2021-07-29 22:01:54 -0300

    Adiciona novas pastas

---
+++ Segunda Semana/29-07-21/Bank/src/CaixaEletronico.java
@@ -0,0 +1,12 @@
+public class CaixaEletronico {
+
+	public static void retirar(int valor) {
+		int notas[] = {50, 20, 10, 2};
+		int resto = valor;
+		int i = 0;
+		int quantidadeNotas[] = new int[notas.length];
+
+		while(resto > notas[i]){
+		}
+	}
+}
