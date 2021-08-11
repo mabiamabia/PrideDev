695344dda15dc0417fdde9cf3436a06e2b5773c5
Mabia <49458473+prisciladuarte@users.noreply.github.com>
2021-08-11 19:23:22 -0300

    immplementa crud

---
+++ Quarta Semana/10-08-21/src/main/java/Exceptions/SaldoInsulficienteExceptions.java
@@ -0,0 +1,11 @@
+package Exceptions;
+
+public class SaldoInsulficienteExceptions {
+
+	
+	private static final long serialVersionUID = 1L;
+	
+	public SaldoInsulficienteException(Sttring mensagemErro) {
+		super(mensagemErro);
+	}
+}
